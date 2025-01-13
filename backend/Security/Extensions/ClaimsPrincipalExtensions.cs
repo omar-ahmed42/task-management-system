664e14ada3445f4f3620d05ab9c690f28f3f4997
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2025-01-13 22:56:03 +0200

    Clean ClaimsPrincipalExtensions

--- backend/Security/Extensions/ClaimsPrincipalExtensions.cs
+++ backend/Security/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,12 +6,17 @@ public static class ClaimsPrincipalExtensions
 {
     public static Guid? GetUserId(this ClaimsPrincipal principal)
     {
-        string id = principal.FindFirstValue(ClaimTypes.NameIdentifier);;
+        string? id = principal.FindFirstValue(ClaimTypes.NameIdentifier);
         return id == null ? null : Guid.Parse(id);
     }
 
-    public static List<string> GetRoles(this ClaimsPrincipal principal) => ((ClaimsIdentity)principal.Identity).Claims.Where(c => c.Type == ClaimTypes.Role)
-            .Select(c => c.Value).ToList();
+    public static List<string> GetRoles(this ClaimsPrincipal principal)
+    {
+        ClaimsIdentity? claimsIdentity = (ClaimsIdentity?) principal.Identity;
+        if (claimsIdentity == null) return [];
+
+        return [.. claimsIdentity.Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value)];
+    }
 
     public static bool HasRole(this ClaimsPrincipal principal, string roleName) => principal.GetRoles().Contains(roleName);
     public static bool IsAdmin(this ClaimsPrincipal principal) => HasRole(principal, "admin");
