a7d40f7593d6a5f3b66ea21b04bf0db7fd7aa841
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2025-01-13 21:01:20 +0200

    Add team entity

---
+++ backend/Entities/Team.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entities;
+
+[Table("teams")]
+public class Team
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public string Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(250)]
+    [Column("description")]
+    public string Description { get; set; }
+
+    [Column("leader_id")]
+    public Guid LeaderId { get; set; }
+
+    [ForeignKey("LeaderId")]
+    public User Leader { get; set; }
+
+}
