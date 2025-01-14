3d497b67281696d2be48661551b094c71c8eef49
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2025-01-14 17:03:37 +0200

    Add an endpoint to retrieve a specific team member

--- backend/Controllers/TeamMemberController.cs
+++ backend/Controllers/TeamMemberController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using backend.Data;
 using backend.Dtos;
+using backend.Dtos.Teams.Members;
 using backend.Entities;
 using backend.Mappers;
 using backend.Security.Extensions;
@@ -17,9 +18,9 @@ namespace backend.Controllers
 
         private readonly TaskManagementDbContext _dbContext = dbContext;
 
-        [HttpGet("teams/{team-id:Guid}/members")]
+        [HttpGet("teams/{team-id:Guid}/members", Name = "GetTeamMembers")]
         [Authorize]
-        public async Task<ActionResult> GetTeamMembers([FromRoute(Name = "team-id")] string teamId)
+        public async Task<ActionResult<List<TeamMemberDetails>>> GetTeamMembers([FromRoute(Name = "team-id")] string teamId)
         {
 
             var team = await _dbContext.Teams.FindAsync(teamId);
@@ -45,10 +46,30 @@ namespace backend.Controllers
             return team != null && team.LeaderId != null && leaderId != null && team.LeaderId.Equals(leaderId);
         }
 
-        private async Task<bool> IsTeamMember(string teamId, Guid? memberId) {
+        private async Task<bool> IsTeamMember(string teamId, Guid? memberId)
+        {
             if (teamId == null || memberId == null) return false;
             var teamMember = await _dbContext.TeamsMembers.FindAsync(teamId, memberId);
             return teamMember != null;
+        }
+
+        [HttpGet("teams/{team-id:Guid}/members/{member-id:Guid}", Name = "GetTeamMember")]
+        [Authorize]
+        public async Task<ActionResult<TeamMemberDetails>> GetTeamMember([FromRoute(Name = "team-id")] string teamId, [FromRoute(Name = "member-id")] Guid memberId)
+        {
+            var team = await _dbContext.Teams.FindAsync(teamId);
+            if (team == null) return NotFound(new ErrorResponse("TEAM_NOT_FOUND", "Team not found"));
+
+            var principalId = User.GetUserId();
+            if (User.IsAdmin() || IsTeamLeader(team, principalId) || await IsTeamMember(teamId, principalId))
+            {
+
+                var teamMember = await _dbContext.TeamsMembers.FindAsync(teamId, memberId);
+                if (teamMember == null) return NotFound(new ErrorResponse("MEMBER_NOT_FOUND", "Team member not found"));
+                return Ok(TeamMemberMapper.ToTeamMemberDetails(teamMember));
+            }
+
+            return StatusCode(403, new ErrorResponse("TEAM_FORBID", "You cannot access this resource"));
         }
     }
 }
